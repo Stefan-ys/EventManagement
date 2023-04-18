8cb2f795fa9b8b98fbb0573afe62c98121fcb838
Stefan-ys <84866588+Stefan-ys@users.noreply.github.com>
2023-04-18 16:39:16 +0300

    Add files via upload

--- eventLog/src/test/java/com/project/eventlog/web/LoginControllerTest.java
+++ eventLog/src/test/java/com/project/eventlog/web/LoginControllerTest.java
@@ -4,11 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,22 +19,25 @@ public class LoginControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    public void testGetLogin() throws Exception {
+    void testGetLoginTest() throws Exception {
         mockMvc.perform(get("/users/login"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("user-auth-login"));
     }
 
-//    @Test
-//    public void testOnFailedLogin() throws Exception {
-//        String username = "testuser";
-//        mockMvc.perform(post("/users/login-error")
-//                        .param(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY, username))
-//                .andExpect(status().is4xxClientError())
-//                .andExpect(status().reason(containsString("Invalid username or password")));
-//                .andExpect(status().is3xxRedirection())
-//                .andExpect(view().name("redirect:/users/login"))
-//                .andExpect(flash().attribute("bad_credentials", true))
-//                .andExpect(flash().attribute("username", username));
-//    }
+    @Test
+    void testOnFailedLoginTest
+            () throws Exception {
+        String username = "testuser";
+        mockMvc.perform(post("/users/login-error")
+                        .param(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY, username)
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/users/login"))
+                .andExpect(flash().attributeExists("bad_credentials"))
+                .andExpect(flash().attributeExists("username"))
+                .andExpect(flash().attribute("bad_credentials", true))
+                .andExpect(flash().attribute("username", username));
+    }
+
 }
